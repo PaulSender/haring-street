e6fbea66c287ad79e87f986e36e76e74f4c5e74c
dylanlazzari <dlazzari@udel.edu>
2023-05-02 10:50:00 -0400

    added fake data

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,33 +7,80 @@ const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
+
+  type Accounts {  #note make a different type name
+    username: String
+    password: String
+  }
+
+  type Recipes{     #note make a different type name
     title: String
     author: String
+    prep_time: Int
+    cook_time: Int
+    total_time: Int
+    ingrediants: String
+    directions: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
+
   type Query {
-    books: [Book]
+    account_info: [Accounts]
+    recipe_data: [Recipes]
   }
 `;
-const books = [
+const account_info = [
     {
-        title: "The Awakening",
-        author: "Kate Chopin",
+        username: "Dlazz",
+        password: "test"
     },
     {
-        title: "City of Glass",
-        author: "Paul Auster",
+        username: "nick",
+        password: "2343"
     },
+    {
+        username: "paul",
+        password: "cookman123"
+    }
+];
+const recipe_data = [
+    {
+        title: "Grilled sausage with potatos and green beans",
+        author: "nick",
+        prep_time: 25,
+        cook_time: 10,
+        total_time: 35,
+        ingrediants: "3 quarters pound fresh green beans, trimmed and halved, half pound red potatoes, quartered",
+        directions: "cook it"
+    },
+    {
+        title: "cheese cake",
+        author: "Dlazz",
+        prep_time: 45,
+        cook_time: 3,
+        total_time: 48,
+        ingrediants: "milk, cheese, cake",
+        directions: "bake it"
+    },
+    {
+        title: "shrimp",
+        author: "paul",
+        prep_time: 10,
+        cook_time: 10,
+        total_time: 20,
+        ingrediants: "shrimp",
+        directions: "stir it"
+    }
 ];
 // Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-        books: () => books,
+        account_info: () => account_info,
+        recipe_data: () => recipe_data
     },
 };
 // The ApolloServer constructor requires two parameters: your schema
